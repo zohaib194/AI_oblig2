0a2542a484c13c986ac3aab61e092289699f415d
zohaib <zohaib_b13@hotmail.com>
2018-09-20 09:19:27 +0200

    Window is made with correct colors

--- Astar.py
+++ Astar.py
@@ -1,4 +1,7 @@
-f = open("board-1-2.txt", "r")
+from tkinter import *
+
+
+f = open("board-1-1.txt", "r")
 print(f.read())
 
 class Enviorment:
@@ -23,12 +26,47 @@ class Enviorment:
 					elif "B" in char:
 						self.grid[i].append(2);
 					else :
-						i+=1;
+						i += 1;
+
+	#def makeGridWindow(self):
+
 
 	def printGrid(self):
 		print(self.grid);
 
+	def makeWindow(self, master):
+		for x in range(0,7):
+			for y in range(0,20):
+				if self.grid[x][y] == 0 :
+					e = Entry(master, text="A", bg ="white", width=2)
+					e.grid(row=x, column=y)
+				elif self.grid[x][y] == -1 :
+					e = Entry(master, bg ="red", width=2)
+					e.grid(row=x, column=y)
+				elif self.grid[x][y] == 1 :
+					e = Entry(master, bg ="green", width=2)
+					e.grid(row=x, column=y)
+				elif self.grid[x][y] == 2 :
+					e = Entry(master, text="A", bg ="black", width=2)
+					e.grid(row=x, column=y)
 
-env = Enviorment();
-env.gridParser("board-1-2.txt");
-env.printGrid();
+
+
+
+def main():
+	env = Enviorment();
+	master = Tk()
+	master.geometry('500x500')
+	master.configure(background='SteelBlue1')
+
+	env.gridParser("board-1-3.txt");
+	env.printGrid();
+	env.makeWindow(master);
+	
+
+	master.mainloop()
+
+
+
+if __name__ == "__main__":
+	main();
